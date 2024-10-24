e0f45816cc3e1b8092770c57a0ace7c5c6c86f3a
Arnoud Devoogdt <arnoud.devoogdt@student.howest.be>
2024-10-24 10:36:26 +0200

    Component implementation in helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,14 +30,33 @@
       Single
     </MultipurposeButton>
   </div>
+
+  <div class="exampleSliders">
+    <!-- Using the slider with an array of values -->
+    <MultipurposeSlider
+      :values="['A1', 'A2', 'B1', 'B2', 'C1', 'C2']"
+      @input="handleSliderInput"
+    />
+
+    <!-- Using the slider with a range -->
+    <MultipurposeSlider
+      :min="0"
+      :max="100"
+      @input="handleSliderInput"
+    />
+  </div>
+
+
 </template>
 
 <script>
 import MultipurposeButton from './MultipurposeButton.vue';
+import MultipurposeSlider from './MultipurposeSlider.vue';
 
 export default {
   components: {
     MultipurposeButton,
+    MultipurposeSlider
   },
   methods: {
     handleLeftButton(isActive) {
@@ -51,7 +70,20 @@ export default {
     },
     handleSingleButton() {
       console.log('Single button clicked')
-    }
+    },
+    handleSliderInput(value) {
+      console.log('Slider value:', value); // Handle the slider value here
+    },
   },
 };
 </script>
+
+<style scoped>
+.exampleSliders {
+  width: 75%; /* Set the width to 75% of the screen */
+  margin: 0 auto; /* Center the slider on the page */
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Ensure sliders are centered within the container */
+}
+</style>

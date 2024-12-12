9ca3583cbfd2dbd02f186689f6564e7a72311be8
Devoogdt Arnoud <arnoud.devoogdt@student.howest.be>
2024-12-12 14:54:03 +0100

    fix overwrite merge conflict

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -213,6 +213,8 @@ export default {
                 additionalParams: this.additionalParamsTextValue,
             };
 
+            this.isLoading = true;
+
             try {
                 const data = await fetchDataFromApi(url, body);
                 this.saveToCache();
@@ -223,11 +225,14 @@ export default {
                 this.showEvaluate = false;
             } catch (error) {
                 this.mainTextValue = 'Error calling API';
+            } finally {
+                this.isLoading = false;
             }
+
         },
         async regenerateText() {
             console.log('Regenerating text');
-            const prompt = "Tweak the main text slightly, based on the following parameters. ONLY OUTPUT THE TEXT, NO OTHER CONTEXT";
+            const prompt = "Tweak the main text slightly, based on the following parameters. Adjust or generate words, grammar or language wherever neccesary to match the parameters as best as possible. ONLY OUTPUT THE TEXT, NO OTHER CONTEXT";
             const url = 'http://localhost:3000/api/' + this.currentModel;
             const body = {
                 prompt,
@@ -238,6 +243,8 @@ export default {
                 mainText: this.mainTextValue,
             };
 
+            this.isLoading = true;
+
             try {
                 const data = await fetchDataFromApi(url, body);
                 this.saveToCache();
@@ -245,11 +252,13 @@ export default {
                 this.showEvaluate = false;
             } catch (error) {
                 this.mainTextValue = 'Error calling API';
+            } finally {
+                this.isLoading = false;
             }
         },
         async evaluateText() {
             console.log('Evaluating text');
-            const prompt = "Evaluate the main Text. PROVIDE A CEFR LEVEL BEFORE THE FEEDBACK, THEN ONLY OUTPUT THE FEEDBACK, NO OTHER CONTEXT";
+            const prompt = "Evaluate the main Text. Give suggestions on how to improve it, but keep it compact and to the point. PROVIDE A CEFR LEVEL BEFORE THE FEEDBACK, THEN ONLY OUTPUT THE FEEDBACK, NO OTHER CONTEXT";
             const url = 'http://localhost:3000/api/' + this.currentModel;
             const body = {
                 prompt,
@@ -257,6 +266,8 @@ export default {
                 additionalParams: this.additionalParamsTextValue,
                 mainText: this.mainTextValue,
             };
+
+            this.isLoading = true;
 
             try {
                 const data = await fetchDataFromApi(url, body);
